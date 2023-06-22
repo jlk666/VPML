ed7a12e1e697946b8e35cd1495261442ac521341
jlk666 <zsliu@ucdavis.edu>
2023-06-22 09:52:37 -0700

    first try genome downloader using py script

--- Genome/Genome_downloader.sh
+++ Genome/Genome_downloader.sh
@@ -1,5 +1,3 @@
-esearch -db assembly -query "Vibrio parahaemolyticus[Organism]" | efetch -format docsum | xtract -pattern DocumentSummary -element AssemblyAccession
-
 esearch -db assembly -query "Vibrio parahaemolyticus[Organism]" | efetch -format docsum | xtract -pattern DocumentSummary -element AssemblyAccession > accessions.txt
 
 ncbi-acc-download -F fasta -o downloaded_genomes accessions.txt
