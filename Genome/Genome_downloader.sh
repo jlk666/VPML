e30e4fda3c90ecd51284a2f2a23b559f588bc4c4
jlk666 <zsliu@ucdavis.edu>
2023-06-23 13:42:19 -0700

    fix issue

--- Genome/Genome_downloader.sh
+++
@@ -1,3 +0,0 @@
-esearch -db assembly -query "Vibrio parahaemolyticus[Organism]" | efetch -format docsum | xtract -pattern DocumentSummary -element AssemblyAccession > accessions.txt
-
-ncbi-acc-download -F fasta -o downloaded_genomes accessions.txt
