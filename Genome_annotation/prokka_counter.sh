96511e61d4b782be2aeacc1e6f5e917f6c8f48ea
jlk666 <zsliu@ucdavis.edu>
2023-08-02 16:19:59 -0700

    add notebook label creator

--- Genome_annotation/prokka_counter.sh
+++ Genome_annotation/prokka_counter.sh
@@ -1,2 +1,4 @@
-count=$(find . -type d ! -path . | wc -l)
-echo "The total number of prokka annotation folders is $count"
+#!/bin/bash
+
+count=$(find /home/zhuosl/VPML/Genome_annotation -type f -name "*.gff" | wc -l)
+echo "The total number of prokka annotation gff is $count"
