27285fb5e9110011542eb56b4eb03fdab5bd6918
jlk666 <zsliu@ucdavis.edu>
2023-08-05 16:06:38 -0700

    selected gff bash script added

---
+++ Genome_annotation/gff_mover.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Source and target directory
+SRC_DIR="/home/zhuosl/VPML/Genome_annotation"
+TARGET_DIR="/home/zhuosl/VPML/Pangenome_construct/Selected_genome_gff"
+
+# Read the CSV file line by line
+while IFS= read -r line
+do
+    # Construct the source path for each .gff file
+    FILE_PATH="$SRC_DIR/prokka_$line/$line.gff"
+
+    # Check if the file exists
+    if [[ -f "$FILE_PATH" ]]; then
+        # Move the file to the target directory
+        mv "$FILE_PATH" "$TARGET_DIR"
+    else
+        # Print a message if the file does not exist
+        echo "File $FILE_PATH does not exist"
+    fi
+done < genome_list_selected.csv
