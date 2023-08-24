88c002681d783d4c8ced9060d1a0aaa03e593204
jlk666 <zsliu@ucdavis.edu>
2023-08-23 23:09:03 -0700

    matrix read change

--- ML_model/matrix_manipulate.py
+++ ML_model/matrix_manipulate.py
@@ -1,15 +1,23 @@
-import numpy as np
-from sklearn.decomposition import PCA
+import sys
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-filename = "genome_matrix_full.csv"
-data_frame = pd.read_csv(filename)
-data_frame = data_frame.set_index('genome_ID')
-features = data_frame.iloc[:, :-1]
-label_mapping = {'Clinical': 1, 'Non_clinical': 0}
-data_frame['Label_numerical'] = data_frame['Label'].map(label_mapping)
-labels = data_frame.iloc[:, -1] 
+def process_genome_matrix(filename):
+    data_frame = pd.read_csv(filename)
+    data_frame = data_frame.set_index('genome_ID')
+    features = data_frame.iloc[:, :-1]
+    label_mapping = {'Clinical': 1, 'Non_clinical': 0}
+    data_frame['Label_numerical'] = data_frame['Label'].map(label_mapping)
+    labels = data_frame.iloc[:, -1] 
 
-print(labels)
+    features_array = features.values
+    labels_array = labels.values
+
+    print("In this pangenome matrix, you have", data_frame.shape[0], "samples and each having", data_frame.shape[1], "features.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python matrix.py <filename>")
+    else:
+        filename = sys.argv[1]
+        process_genome_matrix(filename)
