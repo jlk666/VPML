72d44f8f7204d8d9fcee7d65910fc00a72833cac
jlk666 <zsliu@ucdavis.edu>
2023-08-26 10:32:05 -0700

    DL updated

--- DL_model/DLScript.py
+++ DL_model/DLScript.py
@@ -2,6 +2,14 @@ import sys
 import numpy as np
 import pandas as pd
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
+
 def process_genome_matrix(filename):
     data_frame = pd.read_csv(filename)
     data_frame = data_frame.set_index('genome_ID')
@@ -144,6 +152,44 @@ def ModelEvaluator(model, trainloader, testloader, criterion, optimizer, num_epo
 
     return train_loss_values, train_acc_values, test_acc_values
 
+class CustomMLP(nn.Module):
+    def __init__(self, dropout_prob=0.4):
+        super(CustomMLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, 1400)
+        self.dropout1 = nn.Dropout(dropout_prob)
+        self.fc2 = nn.Linear(1400, 512)
+        self.dropout2 = nn.Dropout(dropout_prob)
+        self.fc3 = nn.Linear(512, 128)
+        self.dropout3 = nn.Dropout(dropout_prob)
+        self.fc4 = nn.Linear(128, output_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout2(x)
+        x = F.relu(self.fc3(x))
+        x = self.dropout3(x)
+        x = self.fc4(x)
+        x = F.softmax(x, dim=1)
+        return x
+
+
+# Define hyperparameters
+input_size = X_train.shape[1]
+output_size = 2
+learning_rate = 0.001
+momentum = 0.9
+num_epochs = 100
+
+# Instantiate the model
+model = CustomMLP()
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -152,15 +198,62 @@ if __name__ == "__main__":
         filename = sys.argv[1]
         model_selection = sys.argv[2]
         model_selection = model_selection.upper()# Make sure capital issue resolved here
-        X,Y = process_genome_matrix(filename)
 
-        if model_selection == 'SVM':
-            SVM(X,Y)
-        elif model_selection == 'RF':
-            RF(X,Y)
-        elif model_selection == 'KNN':
-            KNN(X,Y)
-        elif model_selection == 'ALL':
-            SVM(X,Y)
-            RF(X,Y)
-            KNN(X,Y)
+    # Split data into features and labels
+        features = data_frame.iloc[:, :-2].values  
+        labels = data_frame.iloc[:, -1].values    
+
+    # Define hyperparameters
+        input_size = features.shape[1]
+        output_size = 2
+        learning_rate = 0.001
+        momentum = 0.9
+        num_epochs = 100
+
+        # Define KFold cross-validation
+        k_folds = 5
+        kf = KFold(n_splits=k_folds, shuffle=True, random_state=42)
+
+        # Lists to store results of each fold
+        all_train_loss_values = []
+        all_train_acc_values = []
+        all_val_acc_values = []
+
+        for fold, (train_idx, val_idx) in enumerate(kf.split(features, labels)):
+            print(f'Fold {fold + 1}/{k_folds}')
+
+        # Split data
+        features_train, features_val = features[train_idx], features[val_idx]
+        labels_train, labels_val = labels[train_idx], labels[val_idx]
+
+    # Create datasets for this fold
+        train_dataset = CustomDataset(features_train, labels_train)
+        val_dataset = CustomDataset(features_val, labels_val)
+
+    # Create DataLoaders for this fold
+        trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        valloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    # Instantiate the model
+        model = CustomMLP()
+
+    # Define loss function and optimizer
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+
+    # Evaluate for this fold
+        train_loss_values, train_acc_values, val_acc_values = ModelEvaluator(model, trainloader, valloader, criterion, optimizer, num_epochs)
+
+        all_train_loss_values.append(train_loss_values)
+        all_train_acc_values.append(train_acc_values)
+        all_val_acc_values.append(val_acc_values)
+
+# Average results
+        avg_train_loss = np.mean(all_train_loss_values, axis=0)
+        avg_train_acc = np.mean(all_train_acc_values, axis=0)
+        avg_val_acc = np.mean(all_val_acc_values, axis=0)
+
+# Plot average results
+        figurePlot(num_epochs, avg_train_loss, avg_train_acc, avg_val_acc)
+
+
