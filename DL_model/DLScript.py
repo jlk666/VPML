4ffaadc4ad15a9cab0b43b0741784548c7674259
jlk666 <zsliu@ucdavis.edu>
2023-08-25 21:33:46 -0700

    DL added

---
+++ DL_model/DLScript.py
@@ -0,0 +1,21 @@
+import sys
+import numpy as np
+import pandas as pd
+
+def process_genome_matrix(filename):
+    data_frame = pd.read_csv(filename)
+    data_frame = data_frame.set_index('genome_ID')
+    features = data_frame.iloc[:, :-1]
+    label_mapping = {'Clinical': 1, 'Non_clinical': 0}
+    data_frame['Label_numerical'] = data_frame['Label'].map(label_mapping)
+    labels = data_frame.iloc[:, -1] 
+
+    features_array = features.values
+    labels_array = labels.values
+
+    #Dont need to do data split as we choose to do cross validation 
+    #X_train, X_test, y_train, y_test = train_test_split(features_array, labels_array, test_size=0.2, random_state=42)
+
+    print("In this pangenome matrix, you have", data_frame.shape[0], "samples and each having", data_frame.shape[1], "features.")
+
+    return features_array, labels_array
